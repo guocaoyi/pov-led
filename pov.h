c61e254881962464ae3d9e739e98ab5875e6207c
Yalda <guocaoyi@gmail.com>
2023-10-20 15:04:43 +0800

    feat(main):add pov.h

--- pov.h
+++ pov.h
@@ -1,12 +1,28 @@
-//
-//  pov.h
-//
-//  Created by Yalda on 2013/02
-//
+#ifndef POV_H
+#define POV_H
 
-#ifndef pov_h
-#define pov_h
+#define uchar unsigned char
+#define uint unsigned int
 
-#include <stdio.h>
+// 定义画面切换按键
+#define KEY P3_0
 
-#endif /* pov_h */
+extern uchar KY;
+extern uchar disp;
+extern uchar pic;
+extern uchar num;
+
+extern uchar capricorn[];
+extern uchar hzqian[];
+extern uchar hztian[];
+extern uchar dianchi[];
+
+void display1(void);
+void display2(void);
+void display3(void);
+void display4(void);
+
+void DelayUs(uint N);
+void intersvr0(void);
+
+#endif
